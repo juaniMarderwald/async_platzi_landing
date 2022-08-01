6b560905ef0f2e0948ba9d68a563e0c4a8b6e1a3
Juan Ignacio Marderwald <jmarderwald87@gmail.com>
2022-08-01 15:14:16 -0300

    Add files

---
+++ src/assets/main.js
@@ -0,0 +1,42 @@
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCw05fUBPwmpu-ehXFMqfdMw&part=snippet%2Cid&order=date&maxResults=10';
+
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'f3e36cb1b8mshef0de635b4a41b0p1dc774jsnd7ab8a40bdac',
+        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+    }
+};
+
+async function fetchData(urlApi) {
+    const response = await fetch(urlApi, options);
+    const data = await response.json();
+    return data;
+}
+
+const content = null || document.getElementById('content');
+
+(async () => {
+    try {
+        const videos = await fetchData(API);
+        let view = `
+                    ${videos.items.map(video => `
+                        <div class="group relative">
+                          <div
+                             class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                            <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+                        </div>
+                        <div class="mt-4 flex justify-between">
+                          <h3 class="text-sm text-gray-700">
+                             <span aria-hidden="true" class="absolute inset-0"></span>
+                             ${video.snippet.title}
+                          </h3>
+                        </div>
+                        </div>
+                    `).slice(0, 4).join('')}                    
+        `;
+        content.innerHTML = view; 
+    } catch (error){
+        console.log(error);
+    }
+})();
